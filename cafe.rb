49263fc9824eb7216bbb2e390802f7141cb307c6
Mitsuru Haga <mitsuru.haga@gmail.com>
2022-03-16 12:48:04 +0900

    rubocop 適用

--- cafe.rb
+++ cafe.rb
@@ -5,13 +5,13 @@ DRINKS = [
   { name: 'カフェラテ', price: 400 },
   { name: 'チャイ', price: 460 },
   { name: 'エスプレッソ', price: 340 },
-  { name: '緑茶', price: 450 },
+  { name: '緑茶', price: 450 }
 ].freeze
 
 FOODS = [
   { name: 'チーズケーキ', price: 470 },
   { name: 'アップルパイ', price: 520 },
-  { name: 'ホットサンド', price: 410 },
+  { name: 'ホットサンド', price: 410 }
 ].freeze
 
 def take_order(menus)
